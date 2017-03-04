0e5436aaf675d60e7cfff6f80993e3fcf31994fd
s tech <email@sarcas.tech>
2017-03-04 14:28:13 -0500

    remove unused code in TableBodyCell.js

--- src/table/TableBodyCell.js
+++ src/table/TableBodyCell.js
@@ -2,21 +2,9 @@ import React, { Component } from 'react'
 
 class TableBodyCell extends Component {
 
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      width: 'auto'
-    }
-  }
-
-  updateWidth = (val) => {
-    this.setState({width: val})
-  }
-
   render () {
     return (
-      <td key={this.props.index} width={this.state.width}>{this.props.value}</td>
+      <td key={this.props.index}>{this.props.value}</td>
     )
   }
 }
